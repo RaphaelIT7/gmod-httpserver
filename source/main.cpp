ca88a84c44370c04553577a5b703625077488669
RaphaelIT7 <raphael.i.t@outlook.de>
2023-06-24 04:10:05 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -6,37 +6,71 @@
 #include "cpp-httplib/httplib.h"
 #include "iserver.h"
 #include "threadtools.h"
+#include "unordered_map"
 
 struct ThreadData_t {
 	int port;
+	std::unordered_map<unsigned, httplib::Request> requests;
 };
+ThreadData_t* data;
 
 httplib::Server svr;
 unsigned HTTPServer(void* params)
 {
 	ThreadData_t* vars = (ThreadData_t*)params;
 
+	svr.set_read_timeout(10000);
+	svr.set_keep_alive_timeout(10000);
+	svr.set_write_timeout(10000);
+
 	svr.listen("0.0.0.0", vars->port); // The port for my testserver.
 
 	return 0;
 }
 
+LUA_FUNCTION(Think)
+{
+	if (data->requests.empty()) { return 0; }
+
+	Msg("Request found!");
+	data->requests.clear();
+
+	return 0;
+}
+
+CThreadFastMutex* Mutex = new CThreadFastMutex();
 GMOD_MODULE_OPEN()
 {
 	GlobalLUA = LUA;
 
 	LUA_InitServer(LUA);
 
+	data = new ThreadData_t;
+	data->port = 32039;
+
 	svr.Get("/", [=](const httplib::Request& req, httplib::Response& res) {
-		Msg("Hello World from HTTP");
-		printf("Works");
-		printf(req.remote_addr.c_str());
+		Mutex->Lock();
+		data->requests[data->requests.size() + 1] = req;
+		Mutex->Unlock();
+		ThreadSleep(1000);
 		res.set_content("Hello World", "text/plain");
 	});
 
-	ThreadData_t* data = new ThreadData_t;
-	data->port = 32039;
 	CreateSimpleThread(HTTPServer, data);
+
+	Start_Table(LUA);
+		Add_Func(LUA, Think, "Think");
+	Finish_Table(LUA, "httpserver");
+
+	LUA->PushSpecial(SPECIAL_GLOB);
+	LUA->GetField(-1, "RunString");
+	LUA->PushString(" \
+		hook.Add('Think', 'HTTPServer', function() \
+				httpserver.Think() \
+		end)");
+	LUA->Call(1, 0);
+	LUA->Pop();
+
 
 	return 0;
 }
