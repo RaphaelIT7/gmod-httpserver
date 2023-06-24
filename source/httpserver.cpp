b0d0915d43f9862e1f33a2880f7686bf916d5041
RaphaelIT7 <raphael.i.t@outlook.de>
2023-06-24 08:39:32 +0200

    Update httpserver.cpp

--- source/httpserver.cpp
+++ source/httpserver.cpp
@@ -24,11 +24,42 @@ LUA_FUNCTION(Set_Content)
 
 void CallFunc(int func, httplib::Request request, httplib::Response response)
 {
+	GlobalLUA->ReferencePush(func);
+
+	GlobalLUA->CreateTable();
+
+	GlobalLUA->PushString(request.body.c_str());
+	GlobalLUA->SetField(-2, "body");
+
+	GlobalLUA->PushString(request.remote_addr.c_str());
+	GlobalLUA->SetField(-2, "remote_addr");
+
+	GlobalLUA->PushNumber(request.remote_port);
+	GlobalLUA->SetField(-2, "remote_port");
+
+	GlobalLUA->PushString(request.local_addr.c_str());
+	GlobalLUA->SetField(-2, "local_addr");
+
+	GlobalLUA->PushNumber(request.local_port);
+	GlobalLUA->SetField(-2, "local_port");
+
+	GlobalLUA->PushString(request.method.c_str());
+	GlobalLUA->SetField(-2, "method");
+
+	GlobalLUA->PushNumber(request.authorization_count_);
+	GlobalLUA->SetField(-2, "authorization_count_");
+
+	GlobalLUA->PushNumber(request.content_length_);
+	GlobalLUA->SetField(-2, "content_length_");
+	GlobalLUA->SetField(-2, "");
+
+	GlobalLUA->CreateTable();
+
+	GlobalLUA->PushCFunction(Set_Content);
+	GlobalLUA->SetField(-2, "Set_Content");
 
 	current_response = response;
-
-	GlobalLUA->ReferencePush(func);
-	GlobalLUA->Call(0, 0);
+	GlobalLUA->Call(2, 0);
 }
 
 
